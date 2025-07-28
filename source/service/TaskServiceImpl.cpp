979b1425a579ab2fa90e4c943ece57e87853b3b5
TarkWight <arsentiy.gonda@gmail.com>
2025-07-28 21:19:02 +0600

    Implements Task Service.
    Implements the Task Service, providing an abstraction
    layer for interacting with the storage layer.
    
    This service handles task management operations,
    delegating data access to the underlying storage implementation.

---
+++ source/service/TaskServiceImpl.cpp
@@ -0,0 +1,28 @@
+#include "TaskServiceImpl.hpp"
+
+TaskServiceImpl::TaskServiceImpl(std::shared_ptr<IStorage> storage)
+    : m_storage(std::move(storage)) {}
+
+std::vector<Task> TaskServiceImpl::getAllTasks() const {
+    return m_storage->getAllTasks();
+}
+
+std::optional<Task> TaskServiceImpl::getTaskById(qint64 id) const {
+    return m_storage->getTaskById(id);
+}
+
+qint64 TaskServiceImpl::addTask(const Task &task) {
+    return m_storage->addTask(task);
+}
+
+bool TaskServiceImpl::updateTask(qint64 id, const Task &task) {
+    return m_storage->updateTask(id, task);
+}
+
+bool TaskServiceImpl::deleteTask(qint64 id) {
+    return m_storage->deleteTask(id);
+}
+
+bool TaskServiceImpl::deleteAll() {
+    return m_storage->deleteAll();
+}
