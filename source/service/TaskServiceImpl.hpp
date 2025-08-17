21f6a10dfe5cb12d503c22791f5f337a4606d8a0
TarkWight <arsentiy.gonda@gmail.com>
2025-08-17 07:41:42 +0500

    Update server interface and implementation
    -- replaced qint64 id to quuid
    -- added logger
    -- added logic to server

--- source/service/TaskServiceImpl.hpp
+++ source/service/TaskServiceImpl.hpp
@@ -1,23 +1,28 @@
 #ifndef TASKLIT_SERVICE_TASKSERVICEIMPL_HPP
 #define TASKLIT_SERVICE_TASKSERVICEIMPL_HPP
 
-#include "ITaskService.hpp"
-#include "IStorage.hpp"
+#include <QUuid>
 #include <memory>
+#include <optional>
+#include <vector>
+
+#include "IStorage.hpp"
+#include "ITaskService.hpp"
 
 class TaskServiceImpl : public ITaskService {
 public:
     explicit TaskServiceImpl(std::shared_ptr<IStorage> storage);
 
     std::vector<Task> getAllTasks() const override;
-    std::optional<Task> getTaskById(qint64 id) const override;
-    qint64 addTask(const Task &task) override;
-    bool updateTask(qint64 id, const Task &task) override;
-    bool deleteTask(qint64 id) override;
+    std::optional<Task> getTaskById(const QUuid &taskId) const override;
+
+    QUuid addTask(const Task &task) override;
+    bool updateTask(const QUuid &taskId, const Task &task) override;
+    bool deleteTask(const QUuid &taskId) override;
     bool deleteAll() override;
 
     std::vector<Tag> getAllTags() const override;
-    qint64 addTag(const Tag &tag) override;
+    QUuid addTag(const Tag &tag) override;
 
 private:
     std::shared_ptr<IStorage> m_storage;
